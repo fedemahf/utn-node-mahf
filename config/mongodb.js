cba8603e26d585c7a68e8bb950c43108d7804913
Federico Mahfoud <fedemahf@users.noreply.github.com>
2022-08-14 19:55:39 -0300

    Update mongodb config

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/ecommerce", error => {
-    if (error) {
-        throw error;
-    } else {
-        console.log("MongoDB connected");
-    }
+mongoose.connect("mongodb://localhost/utn-node-mahf-ecommerce", error => {
+  if (error) {
+    throw error;
+  } else {
+    console.log("MongoDB connected");
+  }
 });
 
 module.exports = mongoose;
